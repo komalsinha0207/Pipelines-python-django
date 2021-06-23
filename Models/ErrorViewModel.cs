3972b3e55a5dc93389ddc66fbb91f1e1acea7e4d
Komal Sinha <komalsksinha98@gmail.com>
2021-06-23 07:57:12 +0530

    created a new project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pipelines_python_django_master.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
